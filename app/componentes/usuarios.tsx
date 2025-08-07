399067e69471026c29dd1dadf60222234b5fbb51
Diel-Silva <jardielttz@gmail.com>
2025-08-06 21:56:52 -0300

    2 Aleteração

--- app/componentes/usuarios.tsx
+++ app/componentes/usuarios.tsx
@@ -1,25 +1,69 @@
 'use client'
 import { useEffect, useState } from "react"
 import { usuarioFindAll } from "./lib/api/usuarios"
+import UsuarioEditar from "./usuarioEditar";
 
-const Usuarios = ()=>{
-    const [Usuarios, setUsuarios] = useState<any[]>([])
+const Usuarios = () => {
+    const [usuario, setUsuarios] = useState<any[]>([]);
+    const [usuarioEditar, setUsuarioEditar] = useState<any | null>();
 
-    useEffect(()=>{
-        const fetchBuscaUsuario = async () =>{
+    useEffect(() => {
+        const fetchBuscaUsuario = async () => {
             const response = await usuarioFindAll()
-            if(response){
+            if (response) {
                 setUsuarios(response)
-            }else{
-                return[];
+            } else {
+                return [];
             }
 
         }
         fetchBuscaUsuario()
 
     }, [])
-    return(
-        'usuarios'
+
+
+    function handlerExcluir(id: any): void {
+        alert(id);
+    }
+
+    function handlerEditar(usuario: any): void {
+        setUsuarioEditar(usuario);
+    }
+
+    return (
+        <>
+            <div>
+                <h2>Lista de Usuários</h2>
+                <table border={1} cellPadding={5} cellSpacing={0}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome do usuário</th>
+                            <th>E-mail</th>
+                            <th>Senha</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usuario.map((usuario) => (
+                            <tr key={usuario.id}>
+                                <td>{usuario.id}</td>
+                                <td>{usuario.nome_usua}</td>
+                                <td>{usuario.email_usua}</td>
+                                <td>{usuario.senha_usua}</td>
+                                <td>
+                                    <button onClick={() => handlerEditar(usuario)}>Editar</button>
+                                    <button onClick={() => handlerExcluir(usuario.id)}>Excluir</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {usuarioEditar && (
+                <UsuarioEditar usuario={usuarioEditar} />
+            )}
+        </>
     )
 }
 
